72af111a923c638dfc4b86e2ac17fefed3648bcf
Eniyavan T <eniyavan.thangabharathi@gmail.com>
2026-01-20 15:29:38 +0530

    Update plant_definition.m

--- week01_mass_spring_damper/model/plant_definition.m
+++ week01_mass_spring_damper/model/plant_definition.m
@@ -1,1 +1,9 @@
+% Plant definition: Mass–Spring–Damper system
+
+m = 2;      % mass (kg)
+c = 2;      % damping coefficient (Ns/m)
+k = 4;      % spring constant (N/m)
+
+s = tf('s');
+G = 1 / (m*s^2 + c*s + k);
 
